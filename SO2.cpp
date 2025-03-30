a09f203607b38f462b54cca31346938ec4c80f21
StudentSmerf <272976@student.pwr.edu.pl>
2025-03-30 23:20:31 +0200

    Add unlocking of forks

--- SO2.cpp
+++ SO2.cpp
@@ -8,15 +8,18 @@ using namespace std;
 
 void philosopher(int id, std::mutex* forks, int num_philosophers) {
     int dinner_time = 0;
-    while (dinner_time < 30) {
+    while (dinner_time < 10) {
         std::lock(forks[id], forks[(id + 1) % num_philosophers]);
         this_thread::sleep_for(chrono::milliseconds(1000)); 
         unique_lock<std::mutex> left_fork(forks[id], std::adopt_lock);  
         unique_lock<std::mutex> right_fork(forks[(id + 1) % num_philosophers], std::adopt_lock); 
         
         cout << "Philosopher " << id << " is eating." << endl;
+        
         this_thread::sleep_for(chrono::milliseconds(1000)); 
         cout << "Philosopher " << id << " finished eating." << endl;
+        left_fork.unlock();
+        right_fork.unlock();
         dinner_time++;
     }
 }
