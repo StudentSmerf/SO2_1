929c82f00d2d1f05964900a89ebafcd4d26feea8
StudentSmerf <272976@student.pwr.edu.pl>
2025-03-30 23:16:26 +0200

    Remove debug points and add stop condition

--- SO2.cpp
+++ SO2.cpp
@@ -7,21 +7,17 @@
 using namespace std;
 
 void philosopher(int id, std::mutex* forks, int num_philosophers) {
-    cout << "DP6" << endl;
-    while (true) {
-        cout << "DP7" << endl;
+    int dinner_time = 0;
+    while (dinner_time < 30) {
         std::lock(forks[id], forks[(id + 1) % num_philosophers]);
-        this_thread::sleep_for(chrono::milliseconds(100)); 
-        cout << "DP8" << endl;
+        this_thread::sleep_for(chrono::milliseconds(1000)); 
         unique_lock<std::mutex> left_fork(forks[id], std::adopt_lock);  
-        cout << "DP9" << endl;
         unique_lock<std::mutex> right_fork(forks[(id + 1) % num_philosophers], std::adopt_lock); 
         
-        cout << "DP10" << endl;
         cout << "Philosopher " << id << " is eating." << endl;
-        this_thread::sleep_for(chrono::milliseconds(100)); 
-        cout << "DP11" << endl;
+        this_thread::sleep_for(chrono::milliseconds(1000)); 
         cout << "Philosopher " << id << " finished eating." << endl;
+        dinner_time++;
     }
 }
 
@@ -42,19 +38,14 @@ int main(int argc, char* argv[]) {
         cerr << "Number of philosophers can't be less than 2!" << endl;
         return 1;
     }
-    cout << "DP1" << endl;
     std::mutex* forks = new std::mutex[num_philosophers];  
-    cout << "DP2" << endl;
     thread* threads = new thread[num_philosophers];  
-    cout << "DP3" << endl;
     for (int i = 0; i < num_philosophers; ++i) {
         threads[i] = thread(philosopher, i, forks, num_philosophers);
     }
-    cout << "DP4" << endl;
     for (int i = 0; i < num_philosophers; ++i) {
         threads[i].join();
     }
-    cout << "DP5" << endl;
 
     delete[] forks;
     delete[] threads;
