2a95e47626a6bd361222a1972dfb0e1f9f5c9ca8
StudentSmerf <272976@student.pwr.edu.pl>
2025-03-31 21:09:40 +0200

    Replace resting with thinking

--- SO2.cpp
+++ SO2.cpp
@@ -9,6 +9,8 @@ using namespace std;
 void philosopher(int id, std::mutex* forks, int num_philosophers) {
     int dinner_time = 0;
     while (dinner_time < 10) {
+        cout << "Philosopher " << id << " is thinking." << endl;
+        this_thread::sleep_for(chrono::milliseconds(1000));
         std::lock(forks[id], forks[(id + 1) % num_philosophers]);
         this_thread::sleep_for(chrono::milliseconds(100)); 
         unique_lock<std::mutex> left_fork(forks[id], std::adopt_lock);  
@@ -20,8 +22,7 @@ void philosopher(int id, std::mutex* forks, int num_philosophers) {
         cout << "Philosopher " << id << " finished eating." << endl;
         left_fork.unlock();
         right_fork.unlock();
-        cout << "Philosopher " << id << " is resting." << endl;
-        this_thread::sleep_for(chrono::milliseconds(3000));
+        
         dinner_time++;
     }
 }
