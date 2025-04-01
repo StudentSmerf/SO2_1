65c458054da5bdf9663fff035897b5f62f02942b
StudentSmerf <272976@student.pwr.edu.pl>
2025-04-01 07:22:35 +0200

    Add comments to code

--- SO2.cpp
+++ SO2.cpp
@@ -11,15 +11,20 @@ void philosopher(int id, std::mutex* forks, int num_philosophers) {
     while (dinner_time < 10) {
         cout << "Philosopher " << id << " is thinking." << endl;
         this_thread::sleep_for(chrono::milliseconds(1000));
+
+        //Lock two forks (left and right) using std::lock to avoid deadlock
         std::lock(forks[id], forks[(id + 1) % num_philosophers]);
         this_thread::sleep_for(chrono::milliseconds(100)); 
+
+        //Lock the forks using std::unique_lock to ensure they are unlocked after use
         unique_lock<std::mutex> left_fork(forks[id], std::adopt_lock);  
         unique_lock<std::mutex> right_fork(forks[(id + 1) % num_philosophers], std::adopt_lock); 
         
         cout << "Philosopher " << id << " is eating." << endl;
-        
         this_thread::sleep_for(chrono::milliseconds(1000)); 
+
         cout << "Philosopher " << id << " finished eating." << endl;
+        //Ensure forks are unlocked after eating
         left_fork.unlock();
         right_fork.unlock();
         
@@ -28,8 +33,8 @@ void philosopher(int id, std::mutex* forks, int num_philosophers) {
 }
 
 int main(int argc, char* argv[]) {
-    
     cout << "Dining Philosophers Problem" << endl;
+    //Set number of philosophers
     int num_philosophers = 5;
     try {
         num_philosophers = stoi(argv[1]);
@@ -37,22 +42,29 @@ int main(int argc, char* argv[]) {
         cerr << "Invalid number of philosophers: " << e.what() << endl;
         return 1;
     }
+    
     //check if num_philosophers is set corectly
     cout << "Number of philosophers: " << num_philosophers << endl;
-
     if (num_philosophers < 2) {  
         cerr << "Number of philosophers can't be less than 2!" << endl;
         return 1;
     }
+
+    //Create forks (mutexes) and philosophers (threads)
     std::mutex* forks = new std::mutex[num_philosophers];  
     thread* threads = new thread[num_philosophers];  
+
+    //Create threads for each philosopher
     for (int i = 0; i < num_philosophers; ++i) {
         threads[i] = thread(philosopher, i, forks, num_philosophers);
     }
+
+    //Join threads to ensure they finish before main thread exits
     for (int i = 0; i < num_philosophers; ++i) {
         threads[i].join();
     }
 
+    //Clean up
     delete[] forks;
     delete[] threads;
     cout << "Dinner time is over" << endl;
